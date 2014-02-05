490b3b762a236dc0225048a523f18147e5dd8e40
MGI Admin <mgiadmin@jax.org>
2014-02-05 21:13:12 +0000

    TR11471/GUDMAP2

--- tr11471/tr11471insitu.sh
+++ tr11471/tr11471insitu.sh
@@ -14,6 +14,12 @@ LOG=${PROJECTDIR}/$0.log
 rm -rf ${LOG}
 touch ${LOG}
  
+#
+# IF DOING WI TESTING, RUN:
+#	wi/admin/cleanup
+#
+# TO REMOVE CACHED DATA
+#
 
 cat - <<EOSQL | doisql.csh $MGD_DBSERVER $MGD_DBNAME $0 | tee -a $LOG
 
@@ -56,56 +62,55 @@ then
     echo 'insituload.py failed' >> ${LOG}
     exit 1
 fi
-exit 0
 
 #
 #
 # Associate images with assay results.
 #
-echo "\n`date`" >> ${LOG}
-echo "Associate images with assay results" >> ${LOG}
-${GXDIMAGELOAD}/assocResultImage.py >> ${LOG}
-if [ $? -ne 0 ]
-then
-    echo 'assocResultImage.py failed' >> ${LOG}
-    exit 1
-fi
+#echo "\n`date`" >> ${LOG}
+#echo "Associate images with assay results" >> ${LOG}
+#${GXDIMAGELOAD}/assocResultImage.py >> ${LOG}
+#if [ $? -ne 0 ]
+#then
+#    echo 'assocResultImage.py failed' >> ${LOG}
+#    exit 1
+#fi
 
 #
 # Create the literature index for the new assays.
 #
-echo "\n`date`" >> ${LOG}
-echo "Create the literature index for the new assays" >> ${LOG}
-${ASSAYLOAD}/indexload.py >> ${LOG}
-if [ $? -ne 0 ]
-then
-    echo 'indexload.py failed' >> ${LOG}
-    exit 1
-fi
+#echo "\n`date`" >> ${LOG}
+#echo "Create the literature index for the new assays" >> ${LOG}
+#${ASSAYLOAD}/indexload.py >> ${LOG}
+#if [ $? -ne 0 ]
+#then
+#    echo 'indexload.py failed' >> ${LOG}
+#    exit 1
+#fi
 
 #
 # Reload the MRK_Reference table.
 #
-#echo "\n`date`" >> ${LOG}
-#echo "Reload the MRK_Reference table" >> ${LOG}
-#${MRKCACHELOAD}/mrkref.csh >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'mrkref.csh failed' >> ${LOG}
-#    exit 1
-#fi
+echo "\n`date`" >> ${LOG}
+echo "Reload the MRK_Reference table" >> ${LOG}
+${MRKCACHELOAD}/mrkref.csh >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'mrkref.csh failed' >> ${LOG}
+    exit 1
+fi
 
 #
 # Reload the IMG_Cache table.
 #
-#echo "\n`date`" >> ${LOG}
-#echo "Reload the IMG_Cache table" >> ${LOG}
-#${MGICACHELOAD}/imgcache.csh >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'imgcache.csh failed' >> ${LOG}
-#    exit 1
-#fi
+echo "\n`date`" >> ${LOG}
+echo "Reload the IMG_Cache table" >> ${LOG}
+${MGICACHELOAD}/imgcache.csh >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'imgcache.csh failed' >> ${LOG}
+    exit 1
+fi
 
 date >> ${LOG}
 exit 0
