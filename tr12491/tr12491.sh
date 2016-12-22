21630f44d14337bbcca31c66aea380494a272a6f
Lori Corbani <lori.corbani@jax.org>
2016-12-22 09:40:10 -0500

    TR12491/GXD/Lewandowski et al

---
+++ tr12491/tr12491.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#
+# TR 12491
+#
+# Wrapper script for loading GenePaint dataset into GXD for TR 8270
+#
+
+cd `dirname $0`
+
+. ./tr12491.config
+
+echo ${PROJECTDIR}
+
+LOG=${PROJECTDIR}/$0.log
+rm -rf ${LOG}
+touch ${LOG}
+ 
+date | tee -a $LOG
+ 
+#cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+#EOSQL
+
+date | tee -a $LOG
+
+# Load the probe notes
+#
+#
+echo "Load the probe notes" | tee -a ${LOG}
+${PROBELOAD}/probenotes.py | tee -a ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'probenotes.py failed' | tee -a ${LOG}
+    exit 1
+fi
+
+date | tee -a $LOG
+
+exit 0
