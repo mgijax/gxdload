07c14cf8d255f6a4752b9e77b0b6b351ae1b82b5
Lori Corbani <lori.corbani@jax.org>
2017-09-14 07:36:13 -0400

    TR12649/GUDMAP

--- tr12649/tr12649images.sh
+++ tr12649/tr12649images.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #
-# TR 11471
+# TR 12649
 #
 # Wrapper script for loading
 #
 
 cd `dirname $0`
 
-. ./tr11471.config
+. ./tr12649.config
 
 LOG=${PROJECTDIR}/$0.log
 rm -rf ${LOG}
@@ -17,72 +17,73 @@ touch ${LOG}
 #
 # Copy fullsize images to Pixel DB.
 #
-#echo "\n`date`" >> ${LOG}
-#echo "Copy fullsize to Pixel DB" >> ${LOG}
-#${GXDIMAGELOAD}/pixload.sh >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'pixload.sh failed' >> ${LOG}
-#    exit 1
-#fi
+echo "\n`date`" >> ${LOG}
+echo "Copy fullsize to Pixel DB" >> ${LOG}
+${GXDIMAGELOAD}/pixload.csh >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'pixload.sh failed' >> ${LOG}
+    exit 1
+fi
+exit 0
 
 #
 # Create fullsize image input files for the GXD image load.
 #
-#echo "\n`date`" >> ${LOG}
-#echo "Create fullsize image input files for the GXD image load" >> ${LOG}
-#${GXDIMAGELOAD}/tr11471preFullsize.py >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'tr11471preFullsize.py failed' >> ${LOG}
-#    exit 1
-#fi
+echo "\n`date`" >> ${LOG}
+echo "Create fullsize image input files for the GXD image load" >> ${LOG}
+${GXDIMAGELOAD}/tr12649preFullsize.py >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'tr12649preFullsize.py failed' >> ${LOG}
+    exit 1
+fi
 
 #
 # Create the fullsize image stubs.
 #
-#IMAGEFILE=${IMAGE_FULLSIZE}; export IMAGEFILE
-#IMAGEPANEFILE=${IMAGEPANE_FULLSIZE}; export IMAGEPANEFILE
-#OUTFILE_QUALIFIER=${QUALIFIER_FULLSIZE}; export OUTFILE_QUALIFIER
+IMAGEFILE=${IMAGE_FULLSIZE}; export IMAGEFILE
+IMAGEPANEFILE=${IMAGEPANE_FULLSIZE}; export IMAGEPANEFILE
+OUTFILE_QUALIFIER=${QUALIFIER_FULLSIZE}; export OUTFILE_QUALIFIER
 
-#echo "\n`date`" >> ${LOG}
-#echo "Create the fullsize image stubs" >> ${LOG}
-#${GXDIMAGELOAD}/gxdimageload.py >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'gxdimageload.py failed' >> ${LOG}
-#    exit 1
-#fi
+echo "\n`date`" >> ${LOG}
+echo "Create the fullsize image stubs" >> ${LOG}
+${GXDIMAGELOAD}/gxdimageload.py >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'gxdimageload.py failed' >> ${LOG}
+    exit 1
+fi
 
 #
 # The note load creates output files in the current directory, so go to
 # the directory where the files should be located.
 #
-#cd ${IMAGELOADDATADIR}
+cd ${IMAGELOADDATADIR}
 
 #
 # Load copyright notes.
 #
-#echo "\n`date`" >> ${LOG}
-#echo "Load copyright notes" >> ${LOG}
-#${NOTELOAD}/mginoteload.py -S${MGD_DBSERVER} -D${MGD_DBNAME} -U${MGD_DBUSER} -P${MGD_DBPASSWORDFILE} -I${COPYRIGHTFILE} -M${NOTELOADMODE} -O${IMAGE_OBJECTTYPE} -T${COPYRIGHT_NOTETYPE} >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'mginoteload.py failed' >> ${LOG}
-#    exit 1
-#fi
+echo "\n`date`" >> ${LOG}
+echo "Load copyright notes" >> ${LOG}
+${NOTELOAD}/mginoteload.py -S${MGD_DBSERVER} -D${MGD_DBNAME} -U${MGD_DBUSER} -P${MGD_DBPASSWORDFILE} -I${COPYRIGHTFILE} -M${NOTELOADMODE} -O${IMAGE_OBJECTTYPE} -T${COPYRIGHT_NOTETYPE} >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'mginoteload.py failed' >> ${LOG}
+    exit 1
+fi
 
 #
 # Load caption notes.
 #
-#echo "\n`date`" >> ${LOG}
-#echo "Load caption notes" >> ${LOG}
-#${NOTELOAD}/mginoteload.py -S${MGD_DBSERVER} -D${MGD_DBNAME} -U${MGD_DBUSER} -P${MGD_DBPASSWORDFILE} -I${CAPTIONFILE} -M${NOTELOADMODE} -O${IMAGE_OBJECTTYPE} -T${CAPTION_NOTETYPE} >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'mginoteload.py failed' >> ${LOG}
-#    exit 1
-#fi
+echo "\n`date`" >> ${LOG}
+echo "Load caption notes" >> ${LOG}
+${NOTELOAD}/mginoteload.py -S${MGD_DBSERVER} -D${MGD_DBNAME} -U${MGD_DBUSER} -P${MGD_DBPASSWORDFILE} -I${CAPTIONFILE} -M${NOTELOADMODE} -O${IMAGE_OBJECTTYPE} -T${CAPTION_NOTETYPE} >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'mginoteload.py failed' >> ${LOG}
+    exit 1
+fi
 
 #
 # Create accession IDs that are associated with the fullsize images
@@ -90,10 +91,10 @@ touch ${LOG}
 #
 echo "\n`date`" >> ${LOG}
 echo "Create GUDMAP accession IDs associated with the fullsize images" >> ${LOG}
-${GXDIMAGELOAD}/tr11471imageAssoc.py >> ${LOG}
+${GXDIMAGELOAD}/tr12649imageAssoc.py >> ${LOG}
 if [ $? -ne 0 ]
 then
-    echo 'tr11471imageAssoc.py failed' >> ${LOG}
+    echo 'tr12649imageAssoc.py failed' >> ${LOG}
     exit 1
 fi
 
