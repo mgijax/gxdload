57e96a60bb3c4cf55a171170ca75c13e03b61606
Lori Corbani <lori.corbani@jax.org>
2017-09-14 10:04:07 -0400

    TR12649/GUDMAP

--- tr12649/tr12649images.sh
+++ tr12649/tr12649images.sh
@@ -19,6 +19,26 @@ touch ${LOG}
 #
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
 
+-- will delete these from pixeldb after this is processed on production
+select a.accID
+from IMG_Image i, IMG_ImagePane ii, ACC_Accession a
+where i._Refs_key = 172505
+and i._Image_key = ii._Image_key
+and i._Image_key = a._Object_key 
+and a._MGIType_key = 9 
+and a._LogicalDB_key = 19
+and not exists (select 1
+    from GXD_Assay a, GXD_Specimen s, GXD_InSituResult isr, GXD_InSituResultImage p 
+    where i._Refs_key = a._Refs_key 
+    and a._AssayType_key in (1,6,9,10,11) 
+    and a._Assay_key = s._Assay_key 
+    and s._Specimen_key = isr._Specimen_key 
+    and isr._Result_key = p._Result_key
+    and ii._ImagePane_key = p._ImagePane_key
+)
+order by a.accID
+;
+
 select i._Image_key, i.figureLabel
 into temporary table todelete
 from IMG_Image i, IMG_ImagePane ii
