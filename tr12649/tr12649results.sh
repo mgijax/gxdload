35aa4cf9fbe9a55451947f0b30b7536334b9c54c
Lori Corbani <lori.corbani@jax.org>
2017-10-26 14:07:56 -0400

    TR12649/GUDMAP3

--- tr12649/tr12649results.sh
+++ tr12649/tr12649results.sh
@@ -20,34 +20,35 @@ touch ${LOG}
 # delete results only
 # delete indexes
 #
-cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
-delete from GXD_InSituResult insitu
-using GXD_Assay a, GXD_Specimen s
-where a._Refs_key = 172505
-and a._Assay_key = s._Assay_key
-and s._Specimen_key = insitu._Specimen_key
-;
-delete from GXD_Index where _Refs_key = 172505
-;
-EOSQL
+#cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+#delete from GXD_InSituResult insitu
+#using GXD_Assay a, GXD_Specimen s
+#where a._Refs_key = 172505
+#and a._Assay_key = s._Assay_key
+#and s._Specimen_key = insitu._Specimen_key
+#;
+#delete from GXD_Index where _Refs_key = 172505
+#;
+#EOSQL
 
 # Load the results only
 #
-echo "\n`date`" >> ${LOG}
-echo "Load the results" >> ${LOG}
-${GXDLOAD}/tr12649/resultsload.py >> ${LOG}
-if [ $? -ne 0 ]
-then
-    echo 'resultsload.py failed' >> ${LOG}
-    exit 1
-fi
+#echo "\n`date`" >> ${LOG}
+#echo "Load the results" >> ${LOG}
+#${GXDLOAD}/tr12649/resultsload.py >> ${LOG}
+#if [ $? -ne 0 ]
+#then
+#    echo 'resultsload.py failed' >> ${LOG}
+#    exit 1
+#fi
 
 #
 # Create the literature index for the new assays.
 #
 echo "\n`date`" >> ${LOG}
 echo "Create the literature index for the assays" >> ${LOG}
-${ASSAYLOAD}/indexload.py >> ${LOG}
+#${ASSAYLOAD}/indexload.py >> ${LOG}
+${ASSAYLOAD}/indexload.py
 if [ $? -ne 0 ]
 then
     echo 'indexload.py failed' >> ${LOG}
