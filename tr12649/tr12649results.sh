5c0931d302639e888568f7650e96590b5980e2a0
Lori Corbani <lori.corbani@jax.org>
2017-10-31 07:05:19 -0400

    TR12649/GUDMAP3

--- tr12649/tr12649results.sh
+++ tr12649/tr12649results.sh
@@ -20,35 +20,34 @@ touch ${LOG}
 # delete results only
 # delete indexes
 #
-#cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
-#delete from GXD_InSituResult insitu
-#using GXD_Assay a, GXD_Specimen s
-#where a._Refs_key = 172505
-#and a._Assay_key = s._Assay_key
-#and s._Specimen_key = insitu._Specimen_key
-#;
-#delete from GXD_Index where _Refs_key = 172505
-#;
-#EOSQL
+cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+--delete from GXD_InSituResult insitu
+--using GXD_Assay a, GXD_Specimen s
+--where a._Refs_key = 172505
+--and a._Assay_key = s._Assay_key
+--and s._Specimen_key = insitu._Specimen_key
+--;
+--delete from GXD_Index where _Refs_key = 172505
+--;
+EOSQL
 
 # Load the results only
 #
-#echo "\n`date`" >> ${LOG}
-#echo "Load the results" >> ${LOG}
-#${GXDLOAD}/tr12649/resultsload.py >> ${LOG}
-#if [ $? -ne 0 ]
-#then
-#    echo 'resultsload.py failed' >> ${LOG}
-#    exit 1
-#fi
+echo "`date`" >> ${LOG}
+echo "Load the results" >> ${LOG}
+${GXDLOAD}/tr12649/resultsload.py >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'resultsload.py failed' >> ${LOG}
+    exit 1
+fi
 
 #
 # Create the literature index for the new assays.
 #
-echo "\n`date`" >> ${LOG}
+echo "`date`" >> ${LOG}
 echo "Create the literature index for the assays" >> ${LOG}
-#${ASSAYLOAD}/indexload.py >> ${LOG}
-${ASSAYLOAD}/indexload.py
+${ASSAYLOAD}/indexload.py >> ${LOG}
 if [ $? -ne 0 ]
 then
     echo 'indexload.py failed' >> ${LOG}
@@ -58,12 +57,24 @@ fi
 #
 # Reload the MRK_Reference table.
 #
-echo "\n`date`" >> ${LOG}
+echo "`date`" >> ${LOG}
 echo "Reload the MRK_Reference table" >> ${LOG}
 ${MRKCACHELOAD}/mrkref.csh >> ${LOG}
 if [ $? -ne 0 ]
 then
     echo 'mrkref.csh failed' >> ${LOG}
+    exit 1
+fi
+
+#
+# Reload the GXD_Expression table.
+#
+echo "`date`" >> ${LOG}
+echo "Reload the GXD_Expression table" >> ${LOG}
+${MGICACHELOAD}/gxdexpression.csh >> ${LOG}
+if [ $? -ne 0 ]
+then
+    echo 'gxdexpression.csh failed' >> ${LOG}
     exit 1
 fi
 
