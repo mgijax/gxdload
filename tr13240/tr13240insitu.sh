0cb26aa7c4aebf28e2a90484022beb55a95d1ae2
Lori Corbani <lori.corbani@jax.org>
2021-04-12 07:01:09 -0400

    TR13240/update IMPC lacZ data in GXD/J:228563

--- tr13240/tr13240insitu.sh
+++ tr13240/tr13240insitu.sh
@@ -16,7 +16,7 @@ touch ${LOG}
  
 cd ${ASSAYLOADDATADIR}
 
-date >> ${LOG}
+date | tee -a  ${LOG}
 
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
 delete from GXD_Assay where _Refs_key = 229658 ;
@@ -26,74 +26,66 @@ EOSQL
 #
 # Create the input files for the in situ load.
 #
-echo "\n`date`" >> ${LOG}
-echo "Create the input files for the in situ load" >> ${LOG}
-${PYTHON} ${GXDLOAD}/tr13240/tr13240insitu.py >> ${LOG}
+echo "\n`date`" | tee -a  ${LOG}
+echo "Create the input files for the in situ load" | tee -a  ${LOG}
+${PYTHON} ${GXDLOAD}/tr13240/tr13240insitu.py | tee -a  ${LOG}
 if [ $? -ne 0 ]
 then
-    echo 'tr13240insitu.py failed' >> ${LOG}
+    echo 'tr13240insitu.py failed' | tee -a  ${LOG}
     exit 1
 fi
 
 # Load the assays and results.
 #
-echo "\n`date`" >> ${LOG}
-echo "Load the assays and results" >> ${LOG}
-${PYTHON} ${ASSAYLOAD}/insituload.py >> ${LOG}
+echo "\n`date`" | tee -a  ${LOG}
+echo "Load the assays and results" | tee -a  ${LOG}
+${PYTHON} ${ASSAYLOAD}/insituload.py | tee -a  ${LOG}
 if [ $? -ne 0 ]
 then
-    echo 'insituload.py failed' >> ${LOG}
+    echo 'insituload.py failed' | tee -a  ${LOG}
     exit 1
 fi
-exit 0
 
 #
 # Create the literature index for the new assays.
 #
-echo "\n`date`" >> ${LOG}
-echo "Create the literature index for the new assays" >> ${LOG}
-${PYTHON} ${ASSAYLOAD}/indexload.py >> ${LOG}
+echo "\n`date`" | tee -a  ${LOG}
+echo "Create the literature index for the new assays" | tee -a  ${LOG}
+${PYTHON} ${ASSAYLOAD}/indexload.py | tee -a  ${LOG}
 if [ $? -ne 0 ]
 then
-    echo 'indexload.py failed' >> ${LOG}
+    echo 'indexload.py failed' | tee -a  ${LOG}
     exit 1
 fi
 
 #
 # Reload the GXD_Expression table.
 #
-echo "\n`date`" >> ${LOG}
-echo "Reload the GXD_Expression table" >> ${LOG}
-${MGICACHELOAD}/gxdexpression.csh >> ${LOG}
+echo "\n`date`" | tee -a  ${LOG}
+echo "Reload the GXD_Expression table" | tee -a  ${LOG}
+${MGICACHELOAD}/gxdexpression.csh | tee -a  ${LOG}
 if [ $? -ne 0 ]
 then
-    echo 'gxdexpression.csh failed' >> ${LOG}
+    echo 'gxdexpression.csh failed' | tee -a  ${LOG}
     exit 1
 fi
 
 #
 # Reload the MRK_Reference table.
 #
-echo "\n`date`" >> ${LOG}
-echo "Reload the MRK_Reference table" >> ${LOG}
-${MRKCACHELOAD}/mrkref.csh >> ${LOG}
+echo "\n`date`" | tee -a  ${LOG}
+echo "Reload the MRK_Reference table" | tee -a  ${LOG}
+${MRKCACHELOAD}/mrkref.csh | tee -a  ${LOG}
 if [ $? -ne 0 ]
 then
-    echo 'mrkref.csh failed' >> ${LOG}
+    echo 'mrkref.csh failed' | tee -a  ${LOG}
     exit 1
 fi
 
 #
 # qc_reports.csh
 #
-${GXDLOAD}/tr13240/qcnightly_reports.csh >> ${LOG}
+${GXDLOAD}/tr13240/qcnightly_reports.csh | tee -a  ${LOG}
 
-
-echo "\n`date`" >> ${LOG}
-echo "gxd genotypes that are no longer used" >> ${LOG}
-deletegenotypes.csh
-echo "gxd images that are no longer used" >> ${LOG}
-deleteimages.csh
-
-date >> ${LOG}
+date | tee -a  ${LOG}
 exit 0
